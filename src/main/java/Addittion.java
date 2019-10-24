885620e456d1cb48073002cc2a4925ca18efc2fc
RGhosh-commits <ritwikece5084@gmail.com>
2019-10-24 23:50:03 +0530

    added

---
+++ src/main/java/Addittion.java
@@ -0,0 +1,8 @@
+
+public class Addittion {
+public static int add(int a, int b)
+{
+	int c=a+b;
+	return c;
+}
+}
---
+++ src/test/java/Additiontest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Additiontest {
+
+	@Test
+	public void test() {
+		
+		int result = Addittion.add(10,10);
+		assertEquals(20,result);
+		
+	}
+
+}
